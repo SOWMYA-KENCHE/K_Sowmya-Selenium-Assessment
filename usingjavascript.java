4c7400d09e670b9e42fdbd8a6fe8b02aee063373
sowmya <160483381+SOWMYA-KENCHE@users.noreply.github.com>
2026-09-08 09:39:26 +0530

    Add files via upload

---
+++ usingjavascript.java
@@ -0,0 +1,49 @@
+package assessment;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+
+public class usingjavascript {
+
+	public static void main(String[] args) throws InterruptedException, IOException {
+		// launch the browser
+		WebDriver driver = new ChromeDriver();
+		// maximize the window
+		driver.manage().window().maximize();
+		// implicit wait
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		//application
+		driver.get("https://www.saucedemo.com/");
+		Thread.sleep(1000);
+		//login into the application
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		Thread.sleep(1000);
+		driver.findElement(By.id("login-button")).click();
+		Thread.sleep(4000);
+		//scroll to the bottom
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("window.scrollBy(0,1000)");
+		//takescreenshot
+		TakesScreenshot ts = (TakesScreenshot)driver;
+		File src = ts.getScreenshotAs(OutputType.FILE);
+		File dest = new File("./Ss/Products-page.png");
+		FileHandler.copy(src, dest);
+		Thread.sleep(2000);
+		//quit
+		driver.quit();
+		
+		
+
+	}
+
+}
