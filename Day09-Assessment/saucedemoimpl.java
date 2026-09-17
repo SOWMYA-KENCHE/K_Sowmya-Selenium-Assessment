09181365741aef0855972b790cef85703aabd928
sowmya <160483381+SOWMYA-KENCHE@users.noreply.github.com>
2026-09-17 09:55:31 +0530

    Refactor saucedemoimpl by removing execution logic
    Removed the saucedemo execution logic and main method from the saucedemoimpl file.

---
+++ Day09-Assessment/saucedemoimpl.java
@@ -0,0 +1,44 @@
+package KeywordDriven;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class saucedemoimpl {
+	WebDriver driver;
+	
+	public void launchbrowser()
+	{
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+	
+	public void openurl()
+	{
+		driver.get("https://www.saucedemo.com/");
+	}
+	
+	public void username()
+	{
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+	}
+	
+	public void password()
+	{
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+	}
+	
+	public void loginbutton()
+	{
+		driver.findElement(By.id("login-button")).click();
+	}
+	
+	public void closebrowser()
+	{
+		driver.quit();
+	}
+
+}
