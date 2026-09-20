d007ebb9ae5316fdce1a9e416ca772b6bc2b5e98
sowmya <160483381+SOWMYA-KENCHE@users.noreply.github.com>
2026-09-21 00:02:16 +0530

    Create HRMkeywordexecution.java

---
+++ Day12-Assessment/HRMkeywordexecution.java
@@ -0,0 +1,51 @@
+package orangehrmday12;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+
+public class HRMkeywordexecution {
+	
+	HRMkeywordimplementation k = new HRMkeywordimplementation();
+	
+	public void executors(String keyword) throws IOException, InterruptedException
+	{
+		if(keyword.contains("LAUNCH_BROWSER"))
+		{
+			k.launchbrowser();
+		}
+		if(keyword.contains("OPEN_URL"))
+		{
+			k.openurl();
+		}
+		if(keyword.contains("USERNAME"))
+		{
+			k.username();
+		}
+		if(keyword.contains("PASSWORD"))
+		{
+			k.password();
+		}
+		if(keyword.contains("LOGIN_BUTTON"))
+		{
+			k.loginbutton();
+		}
+		if(keyword.contains("LOGOUT_BUTTON"))
+		{
+			k.logoutbutton();
+		}
+		if(keyword.contains("CLOSE_BROWSER"))
+		{
+			k.closebrowser();
+		}
+	}
+	
+	// HRMkeywordexecution
+	
+	public WebDriver getDriver()
+	{
+	    return k.getDriver();
+	}
+	
+
+}
